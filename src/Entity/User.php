c4e216adae6d2f20c16aad2ef8d86f9a010d5b71
bryanStv <bryans.oyola@gmail.com>
2024-10-10 18:47:14 +0200

    Arreglada validación para que no me deje registrar emails no válidos en servidor

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -18,6 +18,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Assert\Email(message: 'Introduce un correo electrónico válido.')]
     private ?string $email = null;
 
     #[ORM\Column]
